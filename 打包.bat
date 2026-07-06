e42c0b578b3285d36bb34f016593a4ec38d61a88
zsk <573991874@qq.com>
2026-07-06 21:48:43 +0800

    windows 打包

--- 打包.bat
+++ 打包.bat
@@ -9,13 +9,14 @@ echo 正在打包，请稍候...
 echo.
 
 cd /d "%~dp0"
+set ELECTRON_MIRROR=https://npmmirror.com/mirrors/electron/
 call npm run dist
 
 if %errorlevel% equ 0 (
     echo.
     echo ========================================
 echo    打包成功！
-echo    输出目录：dist3\别按那个键 1.0.0.exe
+echo    输出目录：dist3\豚豚·别按那个键 1.0.0.exe
 echo ========================================
 echo.
 pause
